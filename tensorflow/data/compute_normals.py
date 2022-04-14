5fc83be7482bc563df842ec30d81713a296b4c59
franzlaconsay <franzlaconsay11@gmail.com>
2022-04-14 15:19:41 +0800

    update compute normals paths

--- tensorflow/data/compute_normals.py
+++ tensorflow/data/compute_normals.py
@@ -8,8 +8,8 @@ parser.add_argument('--file', type = str, required = True,
                     help = 'ex. --file C:\models\\file.ply')
 args = parser.parse_args()
 
-cloudcompare = args.cloudcompare.replace(os.sep, '/')
-filename = args.file.replace(os.sep, '/')
+cloudcompare = os.path.join(args.cloudcompare)
+filename = os.path.join(args.file)
 current_path = os.path.dirname(filename)
 
 cmd = '%s -silent -c_export_fmt ply -ply_export_fmt ascii -no_timestamp -o %s -octree_normals auto -orient plus_y -model quadric' % (cloudcompare, filename)
